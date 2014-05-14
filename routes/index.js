3d27d33e52d377d5b4969576a778e9b56a2437ee
Shiang-Yun Yang <morris821028@gmail.com>
2014-05-15 06:59:36 +0800

    version 0.1

---
+++ routes/index.js
@@ -0,0 +1,131 @@
+/*
+ * GET home page.
+ */
+var Travis = require('travis-ci');
+var mongoose = require('mongoose');
+var Todo = mongoose.model('Todo');
+
+exports.create = function(req, res) {
+	new Todo({
+		content: req.body.content,
+		updated_at: Date.now()
+	}).save(function(err, todo, count) {
+		res.redirect('/');
+	});
+};
+
+exports.destroy = function(req, res) {
+	Todo.findById(req.params.id, function(err, todo) {
+		todo.remove(function(err, todo) {
+			res.redirect('/');
+		});
+	});
+};
+
+exports.update = function ( req, res ){
+  Todo.findById( req.params.id, function ( err, todo ){
+    todo.content    = req.body.content;
+    todo.updated_at = Date.now();
+    todo.save( function ( err, todo, count ){
+      res.redirect( '/' );
+    });
+  });
+};
+
+exports.edit = function ( req, res ){
+  Todo.
+    find().
+    sort( '-updated_at' ).
+    exec( function ( err, todos ){
+      res.render( 'edit', {
+          title   : 'Express Todo Example',
+          todos   : todos,
+          current : req.params.id
+      });
+    });
+};
+
+exports.repo = function ( req, res ){
+	res.render('repo/index', {
+		title: req.params.id,
+		id: req.params.id,
+		login: false,
+	});
+};
+
+exports.people = function ( req, res ){
+	res.render('people/index', {
+		title: req.params.id,
+		id: req.params.id,
+		login: false,
+	});
+};
+
+exports.login = function ( req, res ){
+	res.render('login/login', {
+		title: req.params.id,
+		id: req.params.id,
+		login: false,
+	});
+};
+
+exports.index = function ( req, res ){
+  Todo.
+    find().
+    sort( '-updated_at' ).
+    exec( function ( err, todos ){
+      res.render( 'index', {
+          title : 'Home',
+          login: false,
+          todos : todos
+      });
+    });
+};
+
+var func3 = function(req, res, callback) {
+	for (var i in res.builds) {
+		for (var j in res.builds[i].job_ids) {
+			console.log(res.builds[i].job_ids[j]);
+			travis.jobs({
+				id: res.builds[i].job_ids[j]
+			}, function(err, res) {
+				console.log(res);
+			});
+		}
+	}
+}
+
+exports.search = function(req, response) {
+	var travis = new Travis({
+		version: '2.0.0'
+	});
+	travis.repos.builds({
+		owner_name: 'mozilla-b2g',
+		name: 'gaia'
+	}, function(err, res) {
+		console.log(res);
+		response.render('search/index', {
+			title: 'Search Page',
+			table_title: 'mozilla-b2g/gaia',
+			builds: res.builds
+		});
+	});
+	// res.redirect( '/' );
+};
+
+exports.job = function(req, response) {
+	var travis = new Travis({
+		version: '2.0.0'
+	});
+	console.log(req.params.id);
+	travis.jobs.log({
+		job_id: req.params.id
+	}, function(err, res) {
+		response.render('job/index', {
+			title: 'Search Page',
+			table_title: 'mozilla-b2g/gaia',
+			log_txt: res
+		});
+	});
+	// res.redirect( '/' );
+};
---
+++ server.js
@@ -0,0 +1,54 @@
+/**
+ * Module dependencies.
+ */
+
+// mongoose setup
+require('./db');
+
+var express = require('express'),
+  routes = require('./routes'),
+  user = require('./routes/user'),
+  http = require('http'),
+  path = require('path'),
+  engine = require('ejs-locals'),
+  Travis = require('travis-ci');
+
+var app = express();
+
+app.configure(function() {
+  app.set('port', process.env.PORT || 8080);
+  app.set('views', __dirname + '/views');
+  // app.set('view engine', 'jade');
+  app.engine('ejs', engine);
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function() {
+  app.use(express.errorHandler());
+});
+
+// app.get('/', routes.index);
+app.get('/', routes.index);
+app.get('/index', routes.index);
+app.post('/search', routes.search);
+app.get('/job/:id', routes.job);
+app.get('/repo/:id', routes.repo);
+app.get('/people/:id', routes.people);
+app.get('/login', routes.login);
+
+app.get('/users', user.list);
+
+app.post('/create', routes.create);
+app.get('/destroy/:id', routes.destroy);
+app.get('/edit/:id', routes.edit);
+app.post('/update/:id', routes.update);
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log("Express server listening on port " + app.get('port'));
+});
